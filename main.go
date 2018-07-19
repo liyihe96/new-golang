339822cb71eff48c113c764d6aeb3f8b8712f893
Ryan Li <liyihe96@gmail.com>
2018-07-19 15:13:17 -0700

    Test

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		log.Printf("title: %s\n", title)
 	}
 
-	fmt.Fprintf(w, "FUCK NEWNEW Hello from:  "+title+"\n")
+	fmt.Fprintf(w, "FUCK FUCK NEWNEW Hello from:  "+title+"\n")
 }
 
 func main() {
